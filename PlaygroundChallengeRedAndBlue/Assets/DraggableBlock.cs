6b362ca96307c0d779a70e679b3122dbcbdbae58
MichaelCourter <michael.courter.64@gmail.com>
2019-04-10 23:33:03 -0700

    Created the pullable block. Saved the test scene as Level1.
    CobblestoneFloor2.png: added to be used by the draggable block.
    
    DraggableBlock: added to allow the Gameobject to be dragged horizontally within a certain range by anything calling its Move function. The Move function just takes the speed as is, so the caller needs to make sure the speed is correct for the amount of times they're called.
    
    Level1: added as a test scene for now, but will be used for the first level of the game.
    
    PullableWall: added to be used in multiple places in a level, can be dragged horizontally within a given range.

---
+++ PlaygroundChallengeRedAndBlue/Assets/DraggableBlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DraggableBlock : MonoBehaviour {
+    [SerializeField] float range;
+    [SerializeField] float startingInterpolationPoint;
+
+    float leftPoint;
+    float rightPoint;
+
+	// Use this for initialization
+	void Start () {
+        float startingPoint = range * startingInterpolationPoint;
+        
+        leftPoint = transform.position.x - startingPoint;
+        rightPoint = leftPoint + range;
+	}
+	
+    public void Move(float speed)
+    {
+        float newXPosition = transform.position.x + speed;
+
+        newXPosition = Mathf.Clamp(newXPosition, leftPoint, rightPoint);
+        
+        transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+    }
+}
