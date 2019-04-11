158e7111458a2b840ce389f5918fe9f693458b91
MichaelCourter <michael.courter.64@gmail.com>
2019-04-10 22:44:30 -0700

    Simple vertical follow camera with test assets.
    BlueSquare.png: added to provide a visual for test leaders.
    
    FollowHighestLeader: added to be used by the camera to center on the highest leader in the array.
    
    manifest: when opening the project, it said the package folder needed updating, so this change occured.

---
+++ PlaygroundChallengeRedAndBlue/Assets/FollowHighestLeader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowHighestLeader : MonoBehaviour {
+    const short DEFAULT_LEADER = 0;
+
+    [SerializeField] GameObject[] leaders;
+    [SerializeField] float smoothTime;
+    [SerializeField] float cameraFollowTriggerPercentage;
+
+    Vector3 dampVelocity = new Vector3();
+
+	// Update is called once per frame
+	void LateUpdate () {
+        Camera thisCamera = GetComponent<Camera>();
+
+        float leadersHeight = -1;
+        GameObject highestLeader = leaders[DEFAULT_LEADER];
+
+//        float verticalScreenRatioPosition = -1;
+
+        for (int i = 1; i < leaders.Length; i++)
+        {
+            leadersHeight = leaders[i].transform.position.y;
+
+            if (leadersHeight > highestLeader.transform.position.y)
+            {
+                highestLeader = leaders[i];
+            }
+        }
+
+//        verticalScreenRatioPosition = (thisCamera.pixelHeight - thisCamera.WorldToScreenPoint(highestLeader.transform.position).y) / thisCamera.pixelHeight;
+
+        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(transform.position.x, highestLeader.transform.position.y, transform.position.z), ref dampVelocity, smoothTime);
+	}
+}
