ef8a3f3ddd57ad83bc1beb1a236fd1d2facdee35
CTThayer <44277556+CTThayer@users.noreply.github.com>
2019-04-11 22:07:45 -0700

    Merged Cody's Work
    Major Changes:
    - The whole playground package had to be imported
    - Custom movement script and swap script were imported as well. Check settings on Player and Player2 for a ballpark (fine tuning still required)
    - Level 2 progress also included
    - Includes wall and floor tiles with collider setups that avoid "sticking"
    
    Work Items:
    - Custom pick-up script not included
    - Camera in level 2 is paired to only Player1 with the "built-in" playground script.  Needs to be swapped for custom camera script
    - Look into fix for "grounc catching" that stops player movement when running.
    - Complete level design

---
+++ PlaygroundChallengeRedAndBlue/Assets/Level2Work/SwapPlaces.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapPlaces : MonoBehaviour {
+
+    private GameObject Player2;
+
+	void Update () {
+		if (Input.GetKeyUp("space")) {
+            Player2 = GameObject.Find("Player2");
+            Vector3 temp = this.transform.position;
+            this.transform.position = Player2.transform.position;
+            Player2.transform.position = temp;
+        }
+	}
+}
