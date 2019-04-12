7a6a2e8ed80ec7b08d16c6dad6a346505669142b
MichaelCourter <michael.courter.64@gmail.com>
2019-04-11 21:08:15 -0700

    Added blocks that can be moved verticaly, like the previous PullableWal.
    DraggableBlock: now inherits from the MovableBlock abstract class. Kept a test Update function in.
    
    Level1: added a WeighableWall for testing.
    
    MovableBlock: added to be the base class of any script that allows the attached gameobject to be moved in a linear line in the given range.
    
    WeighableBlock: added to allow hanging blocks to be pulled down. Inherits from MoveableBlock. Has a test Update function defined.
    
    WeighableWall.prefab: can be pulled down when its Move function is called.

---
+++ PlaygroundChallengeRedAndBlue/Assets/Level1Work/MovableBlock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class MovableBlock : MonoBehaviour
+{
+    [SerializeField] protected float range;
+    [SerializeField] protected float initialInterpolationPoint;
+
+    protected float startingPoint;
+    protected float endPoint;
+
+    // Use this for initialization
+    void Start () {
+        float initialPoint = range * initialInterpolationPoint;
+
+        DerivedClassStart(initialPoint);
+    }
+
+    protected abstract void DerivedClassStart(float startingPoint);
+
+    public abstract void Move(float speed);
+}
--- PlaygroundChallengeRedAndBlue/Assets/Level1Work/WeighableBlock.cs
+++ PlaygroundChallengeRedAndBlue/Assets/Level1Work/WeighableBlock.cs
@@ -2,21 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DraggableBlock : MovableBlock
+public class WeighableBlock : MovableBlock
 {
-	protected override void DerivedClassStart(float initialPoint)
+    protected override void DerivedClassStart(float initialPoint)
     {
-        startingPoint = transform.position.x - initialPoint;
+        startingPoint = transform.position.y - initialPoint;
         endPoint = startingPoint + range;
-	}
-	
+    }
+
     public override void Move(float speed)
     {
-        float newXPosition = transform.position.x + speed;
+        float newXPosition = transform.position.y + speed;
 
         newXPosition = Mathf.Clamp(newXPosition, startingPoint, endPoint);
-        
-        transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+
+        transform.position = new Vector3(transform.position.x, newXPosition, transform.position.z);
     }
 
     private void Update()
